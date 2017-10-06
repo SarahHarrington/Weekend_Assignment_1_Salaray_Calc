c807f53d2cbc8685c8e4522e72cb1286a7405a7a
Sarah Harrington <Sarah.Soberg@gmail.com>
2017-10-06 16:52:56 -0500

    clear function working

--- client.js
+++ client.js
@@ -1,5 +1,30 @@
 $(document).ready(readyNow);
 
+var dataSubmit = []; //array for data submit to DOM
+
 function readyNow(){
+  $('.submitButton').on('click', submitData); //button event
+
+  function submitData(){ //submit function
+    console.log('submit clicked');
+    //variables for array
+    var firstName = $('.firstName').val();
+    var lastName = $('.lastName').val();
+    var idNumber = $('.idNumber').val();
+    var jobTitle = $('.jobTitle').val();
+    var salary = $('.salary').val();
+    //array for submitting data
+    dataSubmit = [firstName, lastName, idNumber, jobTitle, salary];
+    console.log(dataSubmit);
+    //submitting data to DOM
+    $('.employeeDisplay').append(dataSubmit);
+    $('.salaryTotal').append(dataSubmit[4]);//how do I get this to total?
+    clearData();
+    }
+
+      function clearData(){
+        $('.input').val('');
+      
+      }
 
 }
