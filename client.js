d4fef55544b502929fcd1a65c93314901c54d45a
Sarah Harrington <Sarah.Soberg@gmail.com>
2017-10-06 20:11:06 -0500

    working on salary total

--- client.js
+++ client.js
@@ -5,27 +5,21 @@ function readyNow(){
 
   function submitData(){ //submit function
     console.log('submit clicked');
-    //variables for array
+
     var firstName = $('.firstName').val();
     var lastName = $('.lastName').val();
     var idNumber = $('.idNumber').val();
     var jobTitle = $('.jobTitle').val();
-    var salary = $('.salary').val();
+    var salary = parseInt($('.salary').val());
     var tableRow = $('<tr><td>' + firstName + ' ' + lastName + '</td><td>' + idNumber + '</td><td>' + jobTitle + '</td><td>' + '</td><td>' + salary +'</td>');
-    //array for submitting data
-    dataSubmit = [firstName, lastName, idNumber, jobTitle, salary];
-    console.log(dataSubmit);
 
     //submitting data to DOM
     $('.table').append(tableRow);
-
-    //$('.salaryTotal').append(dataSubmit[4]);//how do I get this to total?
+    //$('.salaryTotal').append();
     clearData();
     }
 
-      function clearData(){ //clears form inputs
-        $('.input').val('');
-
-      }
-
+    function clearData(){ //clears form inputs
+      $('.input').val('');
+    }
 }
